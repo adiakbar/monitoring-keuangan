39b89d7d54a1df97caba4e41c662c9a68854b86c
Adi Akbar <adi.akbar91@gmail.com>
2021-01-19 15:29:47 +0700

    update interval update data

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ let app = new Vue({
     section2c: '',
     section3: [{},{}]
   },
+  methods: {
+    startInterval: function() {
+      let self = this;
+      setInterval(function() {
+        axios.get('https://stormy-brushlands-62293.herokuapp.com/api?id=1cmayAQ-yL84zAZMI2c1R-MiEvLK_8CjKrP05qg620Jo&sheet=1')
+          .then(response => {
+            self.section1 = response.data.rows
+          })
+        axios.get('https://stormy-brushlands-62293.herokuapp.com/api?id=1cmayAQ-yL84zAZMI2c1R-MiEvLK_8CjKrP05qg620Jo&sheet=2')
+          .then(response => {
+            self.section2a = response.data.rows.slice(0, 7)
+            self.section2b = response.data.rows.slice(7, 15)
+            self.section2c = response.data.rows.slice(15, 22)
+          })
+        axios.get('https://stormy-brushlands-62293.herokuapp.com/api?id=1cmayAQ-yL84zAZMI2c1R-MiEvLK_8CjKrP05qg620Jo&sheet=3')
+          .then(response => {
+            console.log(response.data.rows)
+            self.section3 = response.data.rows
+          })
+      }, 1000 * 3,6 * 2)
+    }
+  },
   mounted: function() {
     axios.get('https://stormy-brushlands-62293.herokuapp.com/api?id=1cmayAQ-yL84zAZMI2c1R-MiEvLK_8CjKrP05qg620Jo&sheet=1')
       .then(response => {
@@ -23,6 +45,7 @@ let app = new Vue({
         console.log(response.data.rows)
         this.section3 = response.data.rows
       })
+    this.startInterval();
   }
 })
 
